281c80e32152ccdefc97a5e64ab832af0aa37454
MoodyJW <jmoody1813@gmail.com>
2021-12-15 16:09:02 -0600

    added timer that spawns dots instead of on click, only clicked dot moves now, seems functional

--- TODO.md
+++ TODO.md
@@ -3,15 +3,33 @@
 - [x] resume
 - [x] reset
 - [x] game over
-- [ ] maybe inputs for difficulty settings (more dots, less time, etc)
 - [x] disable buttons based on current state (start disabled while running, etc)
 - [x] move dot when it's clicked
 - [x] maybe have chaos setting where all dots move when one is clicked?
 - [ ] styles
+  - [ ] game over animation
+    - [ ] adjust positioning and size
+    - [ ] tune animation timing/positioning
   - [ ] buttons
+    - [x] basic icons
+    - [ ] not sure yet
   - [ ] dots
     - [ ] might add some sort of animation for the timer
+    - [ ] scale based on window size
+    - [ ] play with borders/colors/flashing/etc to make the dots look less terrible
   - [ ] board
     - [x] need to contain the dots
     - [ ] not really sure what it will look like overall
 - [ ] write some tests...
+- [ ] ideas
+  - [ ] only add new dots after so many clicks or some amount of time
+  - [ ] add powerups like +1 to all dots each click or maybe a slow time ability
+  - [ ] inputs for difficulty settings (more dots, less time, etc)
+  - [ ] if adding settings, make a popover or something
+  - [ ] vary size of dots as clicked
+  - [ ] use collision detection to prevent dot overlap
+  - [ ] change game to add dots after n seconds and slowly speed that time up
+        instead of every click adding a dot, remove dots after so many clicks
+        this would mean the user has one dot, clicks it 3 times and it disappears
+        but after 5 seconds, another dot would appear, so at first it would be easy
+        but gets progressively more difficult
--- src/app/services/game.service.ts
+++ src/app/services/game.service.ts
@@ -1,21 +1,33 @@
 import { Injectable } from '@angular/core';
 import { timer, of, Subject, BehaviorSubject, Observable } from 'rxjs';
-import { switchMap, scan, take } from 'rxjs/operators';
+import { switchMap, scan, take, map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
 })
 export class GameService {
-  dots$: Subject<string>[];
+  readonly gameClockMin = -1;
+
+  difficultyLength: number;
+  difficultyTimer: number;
+  gameClock: number;
+  gameClockMax: number;
   timer: number;
   timers: number[];
-  gameIsRunning$ = new BehaviorSubject(false);
+
+  dots$: Subject<string>[];
+  gameClock$: Subject<string> = new Subject();
+  gameClockNeedsReset$ = new BehaviorSubject(false);
   gameIsOver$ = new BehaviorSubject(false);
   gameIsPaused$ = new BehaviorSubject(false);
+  gameIsRunning$ = new BehaviorSubject(false);
   timersNeedReset$ = new BehaviorSubject(false);
 
-  createDots(difficultyLength: number): void {
-    this.dots$ = Array.from({ length: difficultyLength }, () => new Subject());
+  createDots(): void {
+    this.dots$ = Array.from(
+      { length: this.difficultyLength },
+      () => new Subject()
+    );
   }
 
   getTime(index: number, startTime: number): Observable<number | string> {
@@ -24,8 +36,30 @@ export class GameService {
     );
   }
 
+  getGameClock(startTime) {
+    return this.gameClock$.pipe(
+      switchMap((state) => this.checkGameClockState(state, startTime))
+    );
+  }
+
+  setGameDifficulties(
+    gameClockMax: number,
+    difficultyLength: number,
+    difficultyTimer: number
+  ) {
+    this.gameClockMax = gameClockMax;
+    this.difficultyLength = difficultyLength;
+    this.difficultyTimer = difficultyTimer;
+  }
+
+  updateGameClock(currentTime) {
+    this.gameClock = currentTime;
+  }
+
   startGame(): void {
+    this.updateGameClock(this.gameClockMax);
     this.dots$[0].next('start');
+    this.gameClock$.next('start');
     this.gameIsRunning$.next(true);
     this.gameIsPaused$.next(false);
     this.gameIsOver$.next(false);
@@ -39,6 +73,8 @@ export class GameService {
     this.dots$.forEach((dot) => {
       dot.next('pause');
     });
+    this.gameClock$.next('pause');
+    this.gameClockNeedsReset$.next(true);
     this.gameIsPaused$.next(true);
     this.gameIsRunning$.next(false);
     this.timersNeedReset$.next(false);
@@ -53,6 +89,8 @@ export class GameService {
       this.timer = timer + 1;
       this.dots$[index].next('resume');
     });
+    this.gameClock$.next('resume');
+    this.gameClockNeedsReset$.next(true);
     this.gameIsPaused$.next(false);
     this.gameIsRunning$.next(true);
     this.timersNeedReset$.next(false);
@@ -105,5 +143,58 @@ export class GameService {
         return of('0');
     }
     return returnValue;
+  }
+
+  private checkGameClockState(
+    state: string,
+    startTime: number
+  ): Observable<number | string> {
+    let returnValue$: Observable<number | string>;
+    switch (state) {
+      case 'start':
+        const startObs$ = timer(0, 1000).pipe(
+          scan((acc) => --acc, startTime),
+          take(startTime)
+        );
+        const startClock$ = startObs$.pipe(
+          map((secondsRemaining) => {
+            console.log(secondsRemaining);
+            if (secondsRemaining === 0) {
+              debugger;
+              if (this.timers.length < this.difficultyLength) {
+                this.timers.push(this.difficultyTimer);
+                this.gameClock$.next('start');
+              }
+            }
+            return secondsRemaining;
+          })
+        );
+        returnValue$ = startClock$;
+        break;
+      case 'pause':
+        returnValue$ = this.gameClock$;
+        break;
+      case 'resume':
+        console.log(startTime);
+        const resumeObs$ = timer(0, 1000).pipe(
+          scan((acc) => --acc, this.gameClock + 1),
+          take(startTime)
+        );
+        const resumeClock$ = resumeObs$.pipe(
+          map((secondsRemaining) => {
+            if (secondsRemaining === this.gameClockMin) {
+              if (this.timers.length < this.difficultyLength)
+                this.timers.push(this.difficultyTimer);
+              this.gameClock$.next('start');
+            }
+            return secondsRemaining;
+          })
+        );
+        returnValue$ = resumeClock$;
+        break;
+      default:
+        return of('0');
+    }
+    return returnValue$;
   }
 }
